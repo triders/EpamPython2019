52841802bc111ecedcbf5bc68b3b9ad6e0b978e7
Timofey <50168251+triders@users.noreply.github.com>
2019-12-08 23:06:17 +0300

    first upload.

--- 06-advanced-python/hw/task1.py
+++ 06-advanced-python/hw/task1.py
@@ -5,10 +5,43 @@ E - dict(<V> : [<V>, <V>, ...])
 Сделать так, чтобы по графу можно было итерироваться(обходом в ширину)
 """
 
+from collections import deque
+
 
 class Graph:
+
     def __init__(self, E):
         self.E = E
+        self.len = len(E)
+        self.bfs_vertex_list = []
+        self.vertex_set = set()
+        self.queue = deque()
+        self.index = -1
+
+        # find first vertex which has neigh(s)
+        vertexes = set(self.E)
+        while not self.bfs_vertex_list:
+            vertex = vertexes.pop()
+            # if has neigh(s)
+            if self.E[vertex]:
+                self.bfs_vertex_list.append(vertex) # add vertex to ordered list
+                self.queue.extend(self.E[vertex]) # add neigh(s) to queue
+
+        # make full bfs_vertex_list
+        while len(self.bfs_vertex_list) < self.len:
+            vertex = self.queue.popleft()
+            self.queue.extend(self.E[vertex])
+            if vertex not in self.bfs_vertex_list:
+                self.bfs_vertex_list.append(vertex)
+
+    def __next__(self):
+        if self.index + 1 >= self.len:
+            return StopIteration
+        self.index += 1
+        return self.bfs_vertex_list[self.index]
+
+    def __iter__(self):
+        return iter(self.bfs_vertex_list)
 
 
 E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
