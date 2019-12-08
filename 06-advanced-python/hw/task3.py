46f39c81d4888e74cc0abcb403306f32ff73ad81
Timofey <50168251+triders@users.noreply.github.com>
2019-12-08 23:10:19 +0300

    Update task3.py

--- 06-advanced-python/hw/task3.py
+++ 06-advanced-python/hw/task3.py
@@ -7,4 +7,16 @@ print("It's fine")
 
 
 class Suppressor:
-    pass
+    def __init__(self, error_class):
+        self.error_class = error_class
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is self.error_class:
+            print("it's fine")
+            return True
+
+with Suppressor(ZeroDivisionError):
+    n = 1/0
