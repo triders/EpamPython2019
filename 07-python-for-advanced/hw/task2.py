94584a26311678c5c2e811cef279a6efc340110f
Timofey <50168251+triders@users.noreply.github.com>
2019-12-23 22:56:26 +0300

    Update task2.py

--- 07-python-for-advanced/hw/task2.py
+++ 07-python-for-advanced/hw/task2.py
@@ -10,6 +10,55 @@ import time
 import uuid
 
 
+def timer_property(t):
+    class Property:
+        time_interval = t
+
+        def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+            self.fget = fget
+            self.fset = fset
+            self.fdel = fdel
+            if doc is None and fget is not None:
+                doc = fget.__doc__
+            self.__doc__ = doc
+            self.value_set_time = None
+            self.last_value = None
+
+        def __get__(self, obj, objtype=None):
+            if obj is None:
+                return self
+            if self.fget is None:
+                raise AttributeError("unreadable attribute")
+            if self.value_set_time is None or \
+                    time.time() - self.value_set_time > Property.time_interval:
+                self.last_value = self.fget(obj)
+                self.value_set_time = time.time()
+            return self.last_value
+
+        def __set__(self, obj, value):
+            if self.fset is None:
+                raise AttributeError("can't set attribute")
+            self.last_value = value
+            self.value_set_time = time.time()
+            self.fset(obj, value)
+
+        def __delete__(self, obj):
+            if self.fdel is None:
+                raise AttributeError("can't delete attribute")
+            self.fdel(obj)
+
+        def getter(self, fget):
+            return type(self)(fget, self.fset, self.fdel, self.__doc__)
+
+        def setter(self, fset):
+            return type(self)(self.fget, fset, self.fdel, self.__doc__)
+
+        def deleter(self, fdel):
+            return type(self)(self.fget, self.fset, fdel, self.__doc__)
+
+    return Property
+
+
 class Message:
 
     @timer_property(t=10)
@@ -17,15 +66,17 @@ class Message:
         self._msg = self.get_message()
         return self._msg
 
-    @msg.setter # reset timer also
+    @msg.setter  # reset timer also
     def msg(self, param):
-        self._msg = param
+        self.msg = param
 
     def get_message(self):
         """
         Return random string
         """
-        return uuid.uuid4().get_hex()
+        return uuid.uuid4().hex
+
+
 
 
 if __name__ == '__main__':
